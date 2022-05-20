3214d0df1b806ff363071261e58a114c6f50d3d1
mKnopfPortfolio <mknopfportfolio@gmail.com>
2022-05-20 15:19:06 -0400

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Player from "./Components/Player";
+import Song from "./Components/Song";
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
