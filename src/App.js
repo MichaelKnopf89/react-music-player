067be6df6270ab1d259222b589fae83556001cfc
mKnopfPortfolio <mknopfportfolio@gmail.com>
2022-05-22 11:42:48 -0400

    add data/state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, {useState} from "react";
 import Player from "./Components/Player";
 import Song from "./Components/Song";
 import './Styles/App.scss';
+import data from "./data";
 
 function App() {
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+ 
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
